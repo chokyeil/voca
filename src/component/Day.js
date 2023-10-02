992c3b3e164bad9dc9bf3c8e1ae07c977a857769
chokyeil <meme951023@gmail.com>
2023-10-02 22:43:11 +0900

    인터넷 속도가 느릴 때 Loading중 표시 추가

--- src/component/Day.js
+++ src/component/Day.js
@@ -7,8 +7,10 @@ export default function Day() {
   const { day } = useParams();
   const words = useFetch(`http://localhost:3001/words?day=${day}`);
 
-  return <>
+  return (
+  <>
     <h2>Day {day}</h2>
+    {words.length === 0 && <span>Loading...</span>}
     <table>
       <tbody>
         {words.map(word => (
@@ -17,5 +19,6 @@ export default function Day() {
       </tbody>
     </table>
   </>
+  );
 
 }
