a31fc5074a2d7804cb5c2a540294dca142dbbcec
chokyeil <meme951023@gmail.com>
2023-10-02 14:32:20 +0900

    POST, useNavigate()

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import CreateDay from "./component/CreateDay";
+import CreateWord from "./component/CreateWord";
 import Day from "./component/Day";
 import DayList from "./component/DayList";
 import EmptyPage from "./component/EmptyPage";
@@ -14,6 +16,8 @@ function App() {
           <Route exact path="/" element={<DayList />} />
           <Route path="/day/:day" element={<Day />} />
           <Route path="*" element={<EmptyPage />} />
+          <Route path="/create_word" element={<CreateWord />} />
+          <Route path="/create_day" element={<CreateDay />} />
         </Routes>
       </div>
     </BrowserRouter>
